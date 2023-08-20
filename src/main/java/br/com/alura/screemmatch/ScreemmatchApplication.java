a1fc4384037bfcf562925bb9059887c299c52e13
Diego <diegors90@gmail.com>
2023-08-20 15:48:40 -0300

    first commit

---
+++ src/main/java/br/com/alura/screemmatch/ScreemmatchApplication.java
@@ -0,0 +1,19 @@
+package br.com.alura.screemmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreemmatchApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreemmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");		
+	}
+
+}
